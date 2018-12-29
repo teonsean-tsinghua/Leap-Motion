812b8b9cb75829a381c9b6662839c3d1ce3356f8
Bill He <billhebillhe@gmail.com>
2018-12-29 23:07:16 +0800

    added interaction with converter in Sample

---
+++ make.sh
@@ -0,0 +1,3 @@
+make
+install_name_tool -change @loader_path/libLeap.dylib ./lib/libLeap.dylib LeapMotion
+./LeapMotion
--- src/Sample.cpp
+++ src/Sample.cpp
@@ -224,6 +224,17 @@ int main(int argc, char** argv) {
   for(auto each: re) {
     std::cout << each.first << ": " << each.second << std::endl;
   }
+
+  while (true) {
+    std::string user_input;
+    std::cin >> user_input;
+    std::vector<std::pair<std::string, double> > re =
+      converter.convert(user_input);
+    for(auto each: re) {
+      std::cout << each.first << ": " << each.second << std::endl;
+    }
+  }
+
   // // Create a sample listener and controller
   // SampleListener listener;
   // Controller controller;
