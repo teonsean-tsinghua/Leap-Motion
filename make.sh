5d936b3cf00001cb5bffe53d75007ade12221217
Bill He <billhebillhe@gmail.com>
2019-01-01 10:07:07 +0800

    Revert to 812b8b9cb75829a381c9b6662839c3d1ce3356f8

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
-cmake .
 make
 install_name_tool -change @loader_path/libLeap.dylib ./lib/libLeap.dylib LeapMotion
 ./LeapMotion
--- src/Sample.cpp
+++ src/Sample.cpp
@@ -8,10 +8,8 @@
 
 #include <iostream>
 #include <cstring>
-#include <unistd.h>
 #include "Leap.h"
 #include "converter.h"
-#include "keyboardui.h"
 
 using namespace Leap;
 
@@ -38,6 +36,7 @@ const std::string stateNames[] = {"STATE_INVALID", "STATE_START", "STATE_UPDATE"
 void SampleListener::onInit(const Controller& controller) {
   std::cout << "Initialized" << std::endl;
 }
+
 void SampleListener::onConnect(const Controller& controller) {
   std::cout << "Connected" << std::endl;
   controller.enableGesture(Gesture::TYPE_CIRCLE);
@@ -45,13 +44,16 @@ void SampleListener::onConnect(const Controller& controller) {
   controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
   controller.enableGesture(Gesture::TYPE_SWIPE);
 }
+
 void SampleListener::onDisconnect(const Controller& controller) {
   // Note: not dispatched when running in a debugger.
   std::cout << "Disconnected" << std::endl;
 }
+
 void SampleListener::onExit(const Controller& controller) {
   std::cout << "Exited" << std::endl;
 }
+
 void SampleListener::onFrame(const Controller& controller) {
   // Get the most recent frame and report some basic information
   const Frame frame = controller.frame();
@@ -187,12 +189,15 @@ void SampleListener::onFrame(const Controller& controller) {
   }
 
 }
+
 void SampleListener::onFocusGained(const Controller& controller) {
   std::cout << "Focus Gained" << std::endl;
 }
+
 void SampleListener::onFocusLost(const Controller& controller) {
   std::cout << "Focus Lost" << std::endl;
 }
+
 void SampleListener::onDeviceChange(const Controller& controller) {
   std::cout << "Device Changed" << std::endl;
   const DeviceList devices = controller.devices();
@@ -202,9 +207,11 @@ void SampleListener::onDeviceChange(const Controller& controller) {
     std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
   }
 }
+
 void SampleListener::onServiceConnect(const Controller& controller) {
   std::cout << "Service Connected" << std::endl;
 }
+
 void SampleListener::onServiceDisconnect(const Controller& controller) {
   std::cout << "Service Disconnected" << std::endl;
 }
@@ -218,7 +225,6 @@ int main(int argc, char** argv) {
     std::cout << each.first << ": " << each.second << std::endl;
   }
 
-  // Use keyboard inputs to test converter
   while (true) {
     std::string user_input;
     std::cin >> user_input;
